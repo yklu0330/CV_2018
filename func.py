f9b2c5387fa381651062a742a7b2ae0add1908f8
johnson <johnson@johnsondeMacBook-Pro.local>
2018-04-16 18:02:15 +0800

    Add git ignore

--- func.py
+++ func.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sympy import *
 
 
 
@@ -15,6 +16,33 @@ def findHomography(srcPoints, dstPoints):
         [4,5,6],
         [7,8,9]
     ])
+
+    #print(srcPoints[0][0][0])
+
+    x1 = Symbol('x')
+    x2 = Symbol('y')
+    x3 = Symbol('z')
+    x4 = Symbol('a')
+    x5 = Symbol('b')
+    x6 = Symbol('c')
+    x7 = Symbol('d')
+    x8 = Symbol('e')
+
+
+    f1 = None
+    f2 = None
+    f3 = None
+    f4 = None
+    f5 = None
+    f6 = None
+    f7 = None
+    f0 = None
+
+    funcs = [f0, f1, f2, f3, f4, f5, f6, f7]
+    for j in range(4):
+        for i in range(4):
+            funcs[i*2] = srcPoints[j][0][0]*x1 + srcPoints[j][0][1] + z - srcPoints[j][0][0]*dstPoints[j][0][0]*d +
+
 
     return H
 
