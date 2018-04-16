7b50ecbcd1404c9afbde00bcafc1619673887ed0
johnson <johnson@johnsondeMacBook-Pro.local>
2018-04-16 16:54:34 +0800

    Add numpy to func.py

--- func.py
+++ func.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 
 
@@ -10,6 +10,11 @@ def findHomography(srcPoints, dstPoints):
     print(dstPoints)
 
     # calculate homography
+    H = np.array([
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ])
 
     return H
 
