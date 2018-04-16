947da8b2c79354f0ee21d715fb61e190053b53e0
johnson <johnson33.cs04@g2.nctu.edu.tw>
2018-04-16 23:14:51 +0800

    complete find homography 2

--- func.py
+++ func.py
@@ -19,14 +19,7 @@ def findHomography(srcPoints, dstPoints):
 
     #print(srcPoints[0][0][0])
 
-    x = Symbol('0')
-    y = Symbol('1')
-    z = Symbol('2')
-    a = Symbol('3')
-    b = Symbol('4')
-    c = Symbol('5')
-    d = Symbol('6')
-    e = Symbol('7')
+
 
     f1 = None
     f2 = None
@@ -37,35 +30,48 @@ def findHomography(srcPoints, dstPoints):
     f7 = None
     f0 = None
 
+    belem = [0, 0, 0, 0, 0, 0, 0, 0]
+
     funcs = [f0, f1, f2, f3, f4, f5, f6, f7]
     for j in range(4):
-        funcs[j*2] = srcPoints[j][0]*x + srcPoints[j][1]*y + z - srcPoints[j][0]*dstPoints[j][0]*d - dstPoints[j][0]*srcPoints[j][1]*e - dstPoints[j][0]
-        funcs[j*2+1] = srcPoints[j][0]*a + srcPoints[j][1]*b + c - srcPoints[j][0]*dstPoints[j][1]*d - dstPoints[j][1]*srcPoints[j][1]*e - dstPoints[j][1]
+        funcs[j * 2] = [srcPoints[j][0], srcPoints[j][1], 1, 0, 0, 0, -srcPoints[j][0] * dstPoints[j][0],
+                       -dstPoints[j][0] * srcPoints[j][1]]
+        belem[j * 2] = dstPoints[j][0]
+        funcs[j * 2 + 1] = [ 0, 0, 0, srcPoints[j][0], srcPoints[j][1], 1, -srcPoints[j][0] * dstPoints[j][1]
+                           , -dstPoints[j][1] * srcPoints[j][1]]
+        belem[j * 2 + 1] = dstPoints[j][1]
 
-    sol = solve(funcs, x, y, z, a, b, c, d, e)
+    A = np.array([funcs[0], funcs[1], funcs[2], funcs[3], funcs[4], funcs[5], funcs[6], funcs[7]])  # 构造系数矩阵 A
+    b = np.mat(belem).T  # 构造转置矩阵 b （这里必须为列向量）
+    try:
+        sol = np.linalg.solve(A, b)  # 调用 solve 函数求解
+    except np.linalg.LinAlgError:
+        return None
     print(sol)
 
-    H[0, 0] = sol[x].evalf()
-    H[0, 1] = sol[y].evalf()
-    H[0, 2] = sol[z].evalf()
-    H[1, 0] = sol[a].evalf()
-    H[1, 1] = sol[b].evalf()
-    H[1, 2] = sol[c].evalf()
-    H[2, 0] = sol[d].evalf()
-    H[2, 1] = sol[e].evalf()
+    H[0, 0] = sol[0]
+    H[0, 1] = sol[1]
+    H[0, 2] = sol[2]
+    H[1, 0] = sol[3]
+    H[1, 1] = sol[4]
+    H[1, 2] = sol[5]
+    H[2, 0] = sol[6]
+    H[2, 1] = sol[7]
 
     return H
 
 
 def findHomographyRANSAC(srcPoints, dstPoints):
 
-    ITER_NUM = 10
+    ITER_NUM = 500
     bestH = None
     bestErr = np.inf
 
     for x in range(ITER_NUM):
         maybe_idxs, test_idxs = random_partition(4, len(srcPoints))
         H = findHomography(srcPoints[maybe_idxs], dstPoints[maybe_idxs])
+        if H is None:
+            continue
         testErr = np.mean(computeError(H, srcPoints[test_idxs], dstPoints[test_idxs]))
         if testErr < bestErr:
             bestH = H
