4adbaa3ae8f21c37cff57af9546a6bf876a12f5c
Johnson Chao <chao.hs1031@gmail.com>
2018-04-17 00:21:47 +0800

    Remove some extra codes

--- func.py
+++ func.py
@@ -5,59 +5,22 @@ from sympy import *
 
 def findHomography(srcPoints, dstPoints):
 
-    print('src points')
-    print(srcPoints)
-    print('dst points')
-    print(dstPoints)
-
-    # calculate homography
-    H = np.array([
-        [1,2,3],
-        [4,5,6],
-        [7,8,1]
-    ], dtype=np.float)
-
-    #print(srcPoints[0][0][0])
-
-
-
-    f1 = None
-    f2 = None
-    f3 = None
-    f4 = None
-    f5 = None
-    f6 = None
-    f7 = None
-    f0 = None
-
-    belem = [0, 0, 0, 0, 0, 0, 0, 0]
-
-    funcs = [f0, f1, f2, f3, f4, f5, f6, f7]
+    b = np.zeros(8, np.float)
+    A = np.zeros((8, 8), np.float)
     for j in range(4):
-        funcs[j * 2] = [srcPoints[j][0], srcPoints[j][1], 1, 0, 0, 0, -srcPoints[j][0] * dstPoints[j][0],
+        A[j * 2] = [srcPoints[j][0], srcPoints[j][1], 1, 0, 0, 0, -srcPoints[j][0] * dstPoints[j][0],
                        -dstPoints[j][0] * srcPoints[j][1]]
-        belem[j * 2] = dstPoints[j][0]
-        funcs[j * 2 + 1] = [ 0, 0, 0, srcPoints[j][0], srcPoints[j][1], 1, -srcPoints[j][0] * dstPoints[j][1]
+        b[j * 2] = dstPoints[j][0]
+        A[j * 2 + 1] = [ 0, 0, 0, srcPoints[j][0], srcPoints[j][1], 1, -srcPoints[j][0] * dstPoints[j][1]
                            , -dstPoints[j][1] * srcPoints[j][1]]
-        belem[j * 2 + 1] = dstPoints[j][1]
+        b[j * 2 + 1] = dstPoints[j][1]
+    b = b.T
 
-    A = np.array([funcs[0], funcs[1], funcs[2], funcs[3], funcs[4], funcs[5], funcs[6], funcs[7]])  # 构造系数矩阵 A
-    b = np.mat(belem).T  # 构造转置矩阵 b （这里必须为列向量）
     try:
-        sol = np.linalg.solve(A, b)  # 调用 solve 函数求解
+        H = np.linalg.solve(A, b)  # 求解Homography係數
     except np.linalg.LinAlgError:
         return None
-    print(sol)
-
-    H[0, 0] = sol[0]
-    H[0, 1] = sol[1]
-    H[0, 2] = sol[2]
-    H[1, 0] = sol[3]
-    H[1, 1] = sol[4]
-    H[1, 2] = sol[5]
-    H[2, 0] = sol[6]
-    H[2, 1] = sol[7]
-
+    H = np.concatenate([H,[1]]).reshape((3,3))
     return H
 
 
