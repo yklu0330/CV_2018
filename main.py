ff80e3b0998c00bfb29862df11a37718acba8967
Johnson Chao <chao.hs1031@gmail.com>
2018-04-17 13:26:17 +0800

    Find homography with every points in pano

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from glob import glob
 THRESHOLD = 0.7
 
 def main():
-    imgList = glob('./data/yosemite*.jpg')
-    panoFileName = './result/pano_yosemite.jpg'
+    name = 'MelakwaLake'
+    imgList = glob('./data/{}*.png'.format(name))
+    panoFileName = './result/pano_{}.jpg'.format(name)
 
     # Read Images
     Images = {}
@@ -31,58 +32,78 @@ def main():
         PointInImg.update({idx: f})
         Descriptor.update({idx: d})
 
-    # Compute Transformation
-    Transform = {}
-    for idx in range(len(imgList) - 1):
-        print('fitting transformation from ' + str(idx) + ' to ' + str(idx + 1) + '\t')
-        M = SIFTMatcher(Descriptor[idx], Descriptor[idx + 1], THRESHOLD)
+    # Compute homography
+    Transform = {0: np.identity(3)}
+    PointInPano = [PointInImg[0][i].pt for i in range(len(PointInImg[0]))]
+    DescriptorInPano = Descriptor[0]
+    for idx in range(1, len(imgList)):
+        print('fitting transformation from ' + str(idx) + ' to pano\t')
+        M = SIFTMatcher(DescriptorInPano, Descriptor[idx], THRESHOLD)
         print('matching points:', len(M, ), '\n')
 
-        dst_pts = np.float32([PointInImg[idx][m[0]].pt for m in M]).reshape(-1, 1, 2)
-        src_pts = np.float32([PointInImg[idx+1][m[1]].pt for m in M]).reshape(-1, 1, 2)
+        dst_pts = np.float32([PointInPano[m[0]] for m in M]).reshape(-1, 1, 2)
+        src_pts = np.float32([PointInImg[idx][m[1]].pt for m in M]).reshape(-1, 1, 2)
 
         # H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
         H = func.findHomographyRANSAC(src_pts.reshape(-1,2), dst_pts.reshape(-1,2))
-        Transform.update({idx+1: H})
 
-    # Stitch images
-    pano = Images[0]
-    prev_transfrom = np.identity(3, np.float)
-    for idx in range(1, len(imgList)):
-        H = prev_transfrom.dot(Transform[idx])
+        # Remove matched keypoints before appending
+        indices = [m[1] for m in M]
+        pts = np.delete(PointInImg[idx], indices, axis=0)
+        des = np.delete(Descriptor[idx], indices, axis=0)
 
+        # Transform coord. of descriptors to pano space
+        pts = np.asarray([pts[i].pt for i in range(len(pts))])
+        pts = H.dot(np.concatenate([pts.T, np.ones([1, pts.shape[0]])], axis=0))
+        pts = (pts / pts[2])[:2,].T
+
+        # Append points and descriptors
+        PointInPano = np.concatenate([PointInPano, pts], axis=0)
+        DescriptorInPano = np.concatenate([DescriptorInPano, des], axis=0)
+
+        Transform.update({idx: H})
+
+    # Compute translation
+    pano_corners = np.array([[], []])
+    for idx in range(len(imgList)):
         # Find new corners
-        srcImg = Images[idx]
+        curImg = Images[idx]
         corners = np.array([
-            [0, srcImg.shape[1], srcImg.shape[1], 0],
-            [0, 0, srcImg.shape[0], srcImg.shape[0]],
+            [0, curImg.shape[1], curImg.shape[1], 0],
+            [0, 0, curImg.shape[0], curImg.shape[0]],
             [1, 1, 1, 1]
         ])
-        new_corners = H.dot(corners)
-        new_corners = np.concatenate([(new_corners/new_corners[2])[:2,], ((0,pano.shape[1]),(0,pano.shape[0]))], axis=1)
+        new_corners = Transform[idx].dot(corners)
+        pano_corners = np.concatenate([pano_corners, (new_corners/new_corners[2])[:2,]], axis=1)
 
-        # Calculate translation matrix
-        T = np.identity(3, np.float)
-        T[0,2] = -new_corners[0].min()
-        T[1,2] = -new_corners[1].min()
+    # Calculate translation matrix
+    T = np.identity(3, np.float)
+    T[0, 2] = -pano_corners[0].min()
+    T[1, 2] = -pano_corners[1].min()
 
-        # Calculate new image size
-        new_size = (int(new_corners[0].max()-new_corners[0].min()), int(new_corners[1].max()-new_corners[1].min()))
+    # Calculate new image size
+    new_size = (int(pano_corners[0].max()-pano_corners[0].min()), int(pano_corners[1].max()-pano_corners[1].min()))
+
+    # Stitch images
+    pano = np.zeros((new_size[1], new_size[0], 3), np.uint8)
+    for idx in range(len(imgList)):
+
+        H = Transform[idx]
 
         # Warp images
         warped = cv2.warpPerspective(Images[idx], T.dot(H), new_size)
-        pano = cv2.warpPerspective(pano, T, new_size)
 
         # Blend image
-        add_mask = np.sum(warped, axis=2) > np.sum(pano, axis=2)
+        # add_mask = np.sum(warped, axis=2) > np.sum(pano, axis=2)
+        # add_mask = add_mask & (np.sum(pano, axis=2) < 300)
+        result_mask = np.sum(pano, axis=2) != 0
+        temp_mask = np.sum(warped, axis=2) != 0
+        add_mask = temp_mask & (~result_mask)
         for c in range(pano.shape[2]):
             cur_im = pano[:,:,c]
             temp_im = warped[:,:,c]
             cur_im[add_mask] = temp_im[add_mask]
             pano[:,:,c] = cur_im
-
-
-        prev_transfrom = T.dot(H)
 
         cv2.imshow('result{}_{}_pano'.format(idx, idx+1), pano)
         cv2.waitKey(0)
