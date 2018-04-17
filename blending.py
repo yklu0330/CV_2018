Fix image overflow issue

blending.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def MultibandBlending(A, B, mask):
 
-    BAND_NUM = 3
+    BAND_NUM = 6
 
     # generate Mask pyramid
     pyM = [mask]
@@ -51,4 +51,7 @@ def MultibandBlending(A, B, mask):
         res = cv2.resize(ls_, (LS[i].shape[1], LS[i].shape[0]))
         ls_ = cv2.add(res, LS[i])
 
-    return np.asarray(ls_, np.uint8)
+    # Normalize image and convert back to uint8
+    ls_ = cv2.convertScaleAbs(ls_)
+
+    return ls_

main.py
@@ -103,25 +103,9 @@ def main():
         # Do multiband blending
         pano = blending.MultibandBlending(pano, warped, mask)
 
-        # Blend warped image with pano
-        # result_mask = np.sum(pano, axis=2) > 300
-        # temp_mask = np.sum(warped_mask, axis=2) != 0
-        # blend_mask =  temp_mask & result_mask
-        # add_mask = temp_mask & (~result_mask)
-        #
-        # # add_mask = np.sum(warped, axis=2) > np.sum(pano, axis=2)
-        # # add_mask = add_mask & (np.sum(pano, axis=2) < 300)
-        # for c in range(pano.shape[2]):
-        #     cur_im = pano[:,:,c]
-        #     temp_im = warped[:,:,c]
-        #     blend_im = multibandblended[:,:,c]
-        #     cur_im[add_mask] = temp_im[add_mask]
-        #     cur_im[blend_mask] = blend_im[blend_mask]
-        #     pano[:,:,c] = cur_im
-
-        # cv2.imshow('result{}_{}_pano'.format(idx, idx+1), pano)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        cv2.imshow('result{}_{}_pano'.format(idx, idx+1), pano)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     cv2.imwrite(panoFileName, pano)
 